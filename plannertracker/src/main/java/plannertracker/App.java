5b4ae75b85cbb1246f6d4155f78cb286f98bed9a
emanning686 <emanning1030@gmail.com>
2024-02-13 20:44:33 -0500

    init

---
+++ plannertracker/src/main/java/plannertracker/App.java
@@ -0,0 +1,13 @@
+package plannertracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
