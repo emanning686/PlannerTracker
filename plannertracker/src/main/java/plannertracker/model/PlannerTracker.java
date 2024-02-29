824398e9989427a1460b54c477469d946381ef76
emanning686 <emanning1030@gmail.com>
2024-02-29 17:02:54 -0500

    month creating/importing began

--- plannertracker/src/main/java/plannertracker/model/PlannerTracker.java
+++ plannertracker/src/main/java/plannertracker/model/PlannerTracker.java
@@ -10,14 +10,22 @@ import java.util.GregorianCalendar;
 public class PlannerTracker {
     private static final GregorianCalendar calendar = new GregorianCalendar();
     private Month currentMonth;
+    private MonthName currentMonthName;
+    private int currentYear;
     private String tasks;
 
     public PlannerTracker() {
-        // this.currentMonth = getCurrentMonth();
+        this.currentMonth = reloadMonth();
+        this.currentMonthName = getCurrentMonth();
+        this.currentYear = getCurrentYear();
     }
 
     public static MonthName getCurrentMonth() {
         return MonthName.getMonthName(calendar.get(Calendar.MONTH));
+    }
+
+    public static int getCurrentYear() {
+        return calendar.get(Calendar.YEAR);
     }
 
     public void addTask(String name) {
@@ -28,7 +36,28 @@ public class PlannerTracker {
         currentMonth.addHighlight(highlight);
     }
 
-    public void reloadMonth() {
+    public Month importMonth(String fileName) {
+        ;
+    }
+
+    public Month createMonth(String fileName) {
+        ;
+    }
+
+    public Month reloadMonth() {
+        String fileName = this.currentMonthName.name() + this.currentYear + ".csv";
         File dir = new File("plannertracker/src/main/java/plannertracker/data");
+        String contents[] = dir.list();
+        for (String file : contents) {
+            if (file == fileName) {
+                return importMonth(fileName);
+            }
+        }
+        return createMonth(fileName);
+    }
+
+    public static void main(String[] args) {
+        // TESTS
+        System.out.println(calendar.get(Calendar.YEAR));
     }
 }
